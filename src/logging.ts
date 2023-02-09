2affcbe45bea2cf326c0551f2f203086da5e165c
Michael <michaelelgin1@gmail.com>
2023-02-09 16:18:20 -0700

    bugfix logging.ts

--- src/logging.ts
+++ src/logging.ts
@@ -1,7 +1,8 @@
 //File to create a simple logging function to be used elsewhere as desired.
 
-import writeFileSync from 'fs';
+//import writeFileSync as fs from 'fs';
+import * as fs from 'fs';
 
 export const log = (str: string) => {
-	writeFileSync("../logs.txt", str)
+	fs.writeFileSync("../logs.txt", str)
 }
