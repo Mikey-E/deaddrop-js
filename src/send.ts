8a596f7577966c6e2066e1b47b549366cc3e7bed
Michael <michaelelgin1@gmail.com>
2023-02-09 17:52:56 -0700

    log statement for user who doesn't exist

--- src/send.ts
+++ src/send.ts
@@ -4,6 +4,10 @@ import { log } from "./logging";
 
 export const sendMessage = async (user: string) => {
     try {
+
+		//This seems to throw an err and not enter the if-block when there's no user.
+		//But in this assignment i'm not sure if we're supposed to fundamentally change how
+		//deaddrop works other than to add log statements, so I'll leave it alone.
         if (!await userExists(user)) {
             throw new Error("Destination user does not exist");
         }
@@ -12,8 +16,12 @@ export const sendMessage = async (user: string) => {
             await saveMessage(message, user);
         });
 
-
     } catch (error) {
+		//When !await userExists(user) fails.
+		//Ideally I'd try-catch the above if-block specifically but I'm not changing deaddrop's core functionality.
+		//So I'll have to log that this most likely happened bc the user doesn't exist, but not certainly.
+		log("Send-message error to \"" + user + "\" most likely because \"" + user + "\" doesn't exist");
+
         console.error("Error occured creating a new user.", error);
     }
 }
@@ -22,6 +30,5 @@ const getUserMessage = async (): Promise<string> => {
     let rl = readline.createInterface(process.stdin, process.stdout);
     let message: string = await new Promise(resolve => rl.question("Enter your message: ", resolve));
     rl.close();
-	log(message);//@@@
     return message;
 }
